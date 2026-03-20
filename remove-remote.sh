b1bd0defec8dba989b78b2ec3f1848b3872aa8e2
Pavel Valena <pvalena@redhat.com>
2026-03-20 14:23:11 +0100

    remove-remote.sh: also remove skip.txt entries

--- remove-remote.sh
+++ remove-remote.sh
@@ -12,5 +12,9 @@ gitc master
 
 cat ../duplicates.txt | xargs -i zsh -c "echo; echo '>{}<'; gitc '{}' || exit 2; gitcb '{}-duplicate' || exit 255; gituu || exit 255; gitc master|| exit 255; git push -d pvalena '{}'; gitb -D '{}'"
 
+[[ -r skip.txt ]]
+
+cat ../skip.txt | xargs -i zsh -c "echo; echo '>{}<'; gitc '{}' || exit 2; gitcb '{}-skip' || exit 255; gituu || exit 255; gitc master|| exit 255; git push -d pvalena '{}'; gitb -D '{}'"
+
 # Suggestion:
 #   cd .. && ./remove.sh
