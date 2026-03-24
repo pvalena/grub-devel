3dffd73cfcade3a0639cbf3c6476114bae917815
Pavel Valena <pvalena@redhat.com>
2026-03-24 13:22:00 +0100

    Enhance status.sh

--- status.sh
+++ status.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
-set -xe
+set -e
 
 zsh -n "$0"
 
 cd grub
 
-gitb | grep -E '^\s*202' | grep -vE '\-(duplicate|skip)$' | xargs -i zsh -c "echo; echo '>{}<'; gitc '{}' &>/dev/null||exit 255; gits '{}'|cat"
+gitb | grep -E '^\s*202' | grep -vE '\-(duplicate|skip)$' | xargs -i zsh -c "S=\"\$(gitc '{}' &>/dev/null||exit 255; gits '{}' | grep -v '^nothing to commit (use -u to show untracked files)$' | grep -v '^\$' | grep -v '^Your branch is up to date with ' | grep -v '^On branch ')\"; [[ -z \"\$S\" ]] && exit; echo; echo '>{}<'; echo \"\$S\""
 
 gitc master &>/dev/null
