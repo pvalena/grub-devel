6438d60ea9dbb9748e686d20068aba1f13934769
Pavel Valena <pvalena@redhat.com>
2026-03-24 17:26:54 +0100

    Enhance create.sh

--- create.sh
+++ create.sh
@@ -10,6 +10,6 @@ zsh -n "$0"
 # gituf pvalena -o merge_request.create -o merge_request.target=master -o merge_request.target_project=pvalena/grub
 
 gitb | grep -E '^\s*202' | grep -vE '\-(duplicate|skip)$' \
-    | xargs -i zsh -c "echo; echo '>{}<'; gitc '{}' && gituf pvalena -o merge_request.create -o merge_request.target=master -o merge_request.target_project=gnu-grub/grub || exit 255; sleep 5"
+    | xargs -i zsh -c "echo; echo '>{}<'; gitc '{}' || exit 255; gitamah ' ' || exit 255; gituf pvalena -o merge_request.create -o merge_request.target=master -o merge_request.target_project=gnu-grub/grub || exit 255; sleep 5"
 
 gitc master
