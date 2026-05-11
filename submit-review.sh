5a347f638a61c1ee0729b207a6015a68509170d0
Pavel Valena <pvalena@redhat.com>
2026-05-11 15:43:42 +0200

    Enhance submit-review.sh

--- submit-review.sh
+++ submit-review.sh
@@ -4,7 +4,11 @@ set -e
 
 zsh -n "$0"
 
-[[ "$1" == '-n' ]] && { DRY=y; shift||: ; } || DRY=
+[[ "$1" == '-d' ]] && { DEBUG="$1"; set -x; shift||: ; } || DEBUG=
+[[ "$1" == '-n' ]] && { DRY="$1"; shift||: ; } || DRY=
+[[ "$1" == '-v' ]] && { V="$1"; shift||: ; } || V=
+
+[[ -z "$1" ]]
 
 [[ "$(basename "$PWD")" == 'grub' ]] || cd grub
 
@@ -13,6 +17,48 @@ R='../reviews/'
 
 D='../data/done.txt'
 [[ -r "$D" ]]
+
+N='../data/new.txt'
+[[ -r "$N" && -n "$(cat "$N")" ]] && {
+
+    # TODO: unify implementations / wrap in some method
+
+    for m in $(cat $N); do
+
+        [[ -n "$m" ]]
+        b="pr${m}"
+
+        F="${R}${b}.md"
+        [[ -r "$F" ]]
+
+        [[ "$m" == "$(head -n 1 "$F" | grep '^# AI Review: MR' | cut -d'!' -f2 | cut -d' ' -f1)" ]]
+
+        grep -qE "^${m}$" "$D" && continue
+
+        echo -e "\n\n>>> $b: $m\n"
+
+        C="$(cat "$F")"
+        [[ -n "$C" ]]
+
+        [[ -n "$V" ]] && echo "$C" && echo
+
+        [[ -n "$DRY" ]] && continue
+
+        glab mr comment "${m}" --repo gnu-grub/grub -m "${C}"
+
+        echo "$m" >> "$D"
+
+        Z="$(grep -vE "^${m}$" "$N")"
+
+        echo "$Z" > "$N"
+
+        sleep 5
+    done
+
+    exit
+}
+
+# Let's just go through all of them
 
 for F in $(ls -d "${R}"*.md); do
 
