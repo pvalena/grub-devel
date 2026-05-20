cedc0e048f0cc7c8f7b6c1ef7b85b6c794ff4575
Pavel Valena <pvalena@redhat.com>
2026-05-20 02:38:37 +0200

    Enhance pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -18,7 +18,7 @@ run () {
     m="$@"
     echo "> ${m}"
 
-    m="${V}${DEBUG}$m"
+    m="${PRE}${DEBUG}$m"
 
     zsh -n -c "$m"
 
@@ -40,16 +40,20 @@ reviews () {
     echo
 }
 
+## OPTS
+[[ "${1}" == '-c' ]] && { CON="${1}"; shift||:; } || CON=
 [[ "${1}" == '-d' ]] && { DEBUG="echo "; set -x; shift||:; } || DEBUG=
 [[ "${1}" == '-n' ]] && { DRY="${1}"; shift||:; } || DRY=
 [[ "${1}" == '-v' || -n "${DRY}${DEBUG}" ]] && { V='set -x; '; } || V=
 [[ "${1}" == '-v' ]] && { shift||:; }
-
 # For later if we want to run it in a loop or something
 [[ "${1}" == '-w' ]] && { W="$2"; shift 2||:; } || W=0
 
 [[ -z "${1}" ]]
 
+[[ -n "${V}" ]] && { PRE="$V"; V="-v"; } || PRE=
+
+# MAIN
 cd "$(dirname "$0")"
 
 echo
@@ -58,25 +62,26 @@ H='helpers/'
 
 [[ -d "${H}" ]]
 
-# Expects new MRs are present in new.txt; see helpers/watch.sh
+[[ -z "${CON}" ]] && {
 
-run "${H}mr-new.sh"
+    run "${H}mr-new.sh"
 
-run "${H}mr-status-new.sh"
+    run "${H}mr-status-new.sh"
 
-run "${H}checkout-new.sh"
+    run "${H}checkout-new.sh"
 
-run "${H}view-new.sh"
+    run "${H}view-new.sh"
 
-echo -e "\n>>> TODO: Run some Magic AI review here (e.g.: 'claude ...')" >&2
+    echo -e "\n>>> TODO: Run some Magic AI review here (e.g.: 'claude ...')\n" >&2
+}
 
 con "Is the AI review done"
 
-run "./submit-review.sh -n"
+run "./submit-review.sh -n ${V}"
 
 con "Really submit the reviews"
 
-run "./submit-review.sh"
+run "./submit-review.sh ${V}"
 
 
 # Let's handle git as well (not tested)
