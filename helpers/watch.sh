1b8c4e099781677143e9b4af26483ce033f08392
Pavel Valena <pvalena@redhat.com>
2026-06-08 15:56:02 +0200

    watch.sh helper: just monitor for new MRs (old mr-new.sh behaviour)

---
+++ helpers/watch.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/zsh
+
+set -e
+
+zsh -n "$0"
+
+[[ "$(basename "$PWD")" == 'grub' ]] || cd grub
+
+D='../data/done.txt'
+F='../data/closed.txt'
+N='../data/new.txt'
+
+mr="$(cat "$D" "$F" "$N" 2>/dev/null|sort -n|grep -v ^$|tail -n 1)" ||:
+
+[[ -n "$mr" ]] || M=0
+
+while [[ ${mr} -ge 0 ]]; do
+
+    mr=$(($mr+1))
+
+    s="$(glab mr view $mr --repo gnu-grub/grub 2>/dev/null | grep '^state:' | tr -s '\t' ' ' | cut -d' ' -f2)"
+
+    [[ -z "$s" ]] && s='' || \
+        echo ">>> $mr: $s"
+
+    case $s in
+        open)
+            # Do not write New -- obsoleted by mr-new.sh (watching for labels)
+#            echo "$mr" >> "$N"
+            ;;
+
+        merged|closed)
+            echo "$mr" >> "$F"
+            ;;
+
+        '')
+            exit
+            ;;
+
+        *)
+            echo "=> IDK">&2
+            exit 2;;
+
+    esac
+
+    sleep 0.5
+done
+
+echo "ERROR: mr|$mr OOB">&2
+exit 3
